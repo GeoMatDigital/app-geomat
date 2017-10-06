22dc06f9e04c239f13967810642eb7363df35fe0
Bastian Krones <bkrones.uni@gmail.com>
2017-10-06 13:26:18 +0200

    Cleaned up some code

--- src/models/mineral_type.ts
+++ src/models/mineral_type.ts
@@ -1,17 +1,15 @@
-
 /**
- * Model of a mineral
+ * Interface of type of mineral
  */
-export class Mineral_Type  {
-  constructor(
+export interface MineralType  {
     /**
      * id of mineral
      */
-    public id: number,
+    id: number,
     /**
      * classification-object of mineral (currently only used for 'silikate')
      */
-    public classification: {
+    classification: {
       /**
        * classification of mineral (currently only used for 'Silikate')
        */
@@ -20,70 +18,70 @@ export class Mineral_Type  {
     /**
      * trivia name of mineral
      */
-    public trivial_name: string,
+    trivial_name: string,
     /**
      * main category of minerals (e.g. 'Silikate und Germanate' or 'Carbonate und Nitrate', etc.)
      */
-    public systematics: string,
+    systematics: string,
     /**
      * variety of the mineral (e.g.: "Bronzit", "Adular", "Fuchsit")
      */
-    public variety: string,
+    variety: string,
     /**
      * Alternative name to trivia name?
      */
-    public minerals: string,
+    minerals: string,
     /**
      * toughness of mineral
      */
-    public mohs_scale: string,
+    mohs_scale: string,
     /**
      * density of mineral
      */
-    public density: string,
+    density: string,
     /**
      * streak of mineral
      */
-    public streak: string,
+    streak: string,
     /**
      * common color of mineral
      */
-    public normal_color: string,
+    normal_color: string,
     /**
      * fracture of mineral
      */
-    public fracture: string[],
+    fracture: string[],
     /**
      * cleavage of mineral
      */
-    public cleavage: string[],
+    cleavage: string[],
     /**
      * lustre of mineral
      */
-    public lustre: string[],
+    lustre: string[],
     /**
      * chemical formula of mineral
      */
-    public chemical_formula: string,
+    chemical_formula: string,
     /**
      * additional info of mineral (rarely used)
      */
-    public other: string,
+    other: string,
     /**
      * external-link to [Mindat]{@link http://www.mindat.org}
      */
-    public resource_mindat: string,
+    resource_mindat: string,
     /**
      * Links to external reference of mineral to [Mineralienatlas]{@link https://www.mineralienatlas.de}
      */
-    public resource_mineralienatlas: string,
+    resource_mineralienatlas: string,
     /**
      * date of mineral's creation
      */
-    public created_at: string,
+    created_at: string,
     /**
      * date of mineral's last modification
      */
-    public last_modified: string
-  ) {}
+    last_modified: string
 }
+
--- src/models/profile.ts
+++ src/models/profile.ts
@@ -1,30 +1,29 @@
-import { Mineral_Type } from './mineral_type';
+import { MineralType } from './mineral_type';
 
 /**
- * Model of mineral of profile-detail page
+ * Interface of mineral of profile-detail page
  */
-export class Profile {
-  constructor(
+export interface Profile {
     /**
      * id of mineral
      */
-    public id: number,
+    id: number,
     /**
      * type of mineral
      *
      * [Mineral_Type](@link Mineral_Type)
      */
-    public mineral_type: Mineral_Type,
+    mineral_type: MineralType,
     /**
      * type of crystal-system's abbreviation (e.g. 'TR' for trigonal', 'HG' for 'hexagonal')
      */
-    public crystal_system: string,
+    crystal_system: string,
     /**
      * temperature of mineral (currently not used)
      */
-    public temperature: string,
+    temperature: string,
     /**
      * pressure of mineral (currently not used)
      */
-    public pressure: string){}
+    pressure: string
 }
