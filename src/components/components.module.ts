ce30379a18b3e3b10ec88f0a7b927d76d9f36ac2
Hassan <H.H.Hatoum@stud.uni-frankfurt.de>
2017-12-04 21:52:29 +0100

    quiz added

---
+++ src/components/components.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { FlashCard } from './flash-card/flash-card';
+@NgModule({
+	declarations: [FlashCard],
+	imports: [],
+	exports: [FlashCard]
+})
+export class ComponentsModule {}
---
+++ src/providers/quiz-data/quiz-data.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+/*
+  Generated class for the QuizDataProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class QuizDataProvider {
+  data: any;
+
+  constructor(public http: Http) {
+
+  }
+
+  load(){
+
+    if(this.data){
+      return Promise.resolve(this.data);
+    }
+
+    return new Promise(resolve => {
+
+      this.http.get('https://geomat.uni-frankfurt.de/api/quizquestion_list').map(res => res.json()).subscribe(data => {
+        this.data = data;
+        resolve(this.data);
+      });
+
+    });
+
+  }
+
+}
