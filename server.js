7b1241432cbc463273bc50da5e7b6a8fd6d4fab9
Michael Gecht <gecht.m@gmail.com>
2017-12-20 09:56:37 +0100

    Update express server

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
-const express = require('express')
+const express = require('express');
 
-const app = express()
-const port = process.env.PORT || 5000
-const router = express.Router()
+const app = express();
+const port = process.env.PORT || 5000;
+const router = express.Router();
 
-app.use(express.static(`${__dirname}/www`))
+app.use(express.static(`${__dirname}/www`));
 
-app.set('views', `${__dirname}/www`)
+app.set('views', `${__dirname}/www`);
 
 router.get('/*', (req, res, next) => {
   res.sendFile(`${__dirname}/www/index.html`)
-})
+});
 
-app.use('/', router)
+app.use('/', router);
 
-app.listen(port)
-console.log('Ionic app running on port', port)
+app.listen(port, function () {
+  console.log('Ionic app running on port ', port);
+});
